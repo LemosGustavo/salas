bd2b5ab99b20ff8dc26a2d038600e9be79a63015
GustavoLemos <gustavolemos089@gmail.com>
2022-05-15 15:49:40 -0300

    Base AdminLte 3.0

--- app/Controllers/SalasController.php
+++ app/Controllers/SalasController.php
@@ -6,6 +6,19 @@ class SalasController extends BaseController
 {
     public function index()
     {
-        echo "Hola";
+        $this->loadViews("salas/index");
+    }
+
+    public function loadViews($view = null, $data = null) {
+        if ($data) {
+            $data['view'] = $view;
+            echo view("includes/header", $data);
+            echo view($view, $data);
+            echo view("includes/footer", $data);
+        } else {
+            echo view("includes/header");
+            echo view($view);
+            echo view("includes/footer");
+        }
     }
 }
